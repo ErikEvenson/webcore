f67dfeab2a8034e63a5bff96233110a018700ccd
Erik Evenson <erik.e.evenson@gmail.com>
2015-01-24 17:35:35 -0600

    Remove obsolete code

--- puppet/local_modules/node_globals/manifests/init.pp
+++ puppet/local_modules/node_globals/manifests/init.pp
@@ -9,18 +9,4 @@ class node_globals {
   package {'fontconfig':
     ensure => installed
   }
-
-#  $node_globals_array = [
-#    'yo@1.3.3',
-#  ]#
-
-#  $node_globals = join($node_globals_array, ' ')#
-
-#  exec {'npm_globals':
-#    command => "/usr/bin/npm config set prefix /home/vagrant/.npm \
-#      && npm -g install ${node_globals}",#
-
-#    require => Package['fontconfig'],
-#    timeout => 0,
-#  }
 }
